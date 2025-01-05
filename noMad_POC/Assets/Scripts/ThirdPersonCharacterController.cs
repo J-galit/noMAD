a3a944d2ce28b3a4167b7751952473828c37982a
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-01-04 22:13:31 -0500

    Three adaptations added
    -Jumpboost: increased jump height when active
    -Speedboost: increased movement speed when active
    -SmallerSize: size of players is reduced when active

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:872d0f5a2cbbe2cb7ef61dd179de6d03b223813261af24a1f003073bf4f4db41
-size 3396
+oid sha256:69094871bdee27860e26cf34a7cf2b6314b720e0be0f73ff13eeff7752797ebe
+size 5116
