04f2467a0f8d18f740b793175f7797711d834b6e
J-galit <jaygalita@gmail.com>
2025-01-06 16:42:14 -0500

    Changed the Collision Hitbox on the character for more consistent jumps

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e5dfdfb02a5483a6f051d2011e12c3b61ff6cb18a6cdc908848338a2d1075c79
-size 6058
+oid sha256:f30cfa129b4a23a1ffefedec6211f50900f31b2f1b9caa203021dd03cd2e022c
+size 6123
