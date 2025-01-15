82f6fbe561421cdb63116ed9f9d74a15d828c00c
J-galit <jaygalita@gmail.com>
2025-01-15 11:00:50 -0500

    Hid the Cursor
    I hid the cursor during normal gameplay but it reappears on the adaptations select screen. I also adjusted the players collision to make jumping more consistent.

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:130b0f541fd31a74b6540b2c0819950408fe5d21ca6ed4897e7ecf0d2bdb491e
-size 6829
+oid sha256:e4ac751abc5393d7ab05d6c4b4676357b8f5c72e6fa531e3662481bab954d41b
+size 6979
