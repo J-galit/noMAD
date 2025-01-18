555a09426cd1d34b4b8b6356a38f25c0be78ce07
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-01-18 00:06:57 -0500

    Money Bug AI is Functional
    -Added NodeCanvas
    
    Wander State is not an action task but a regular script
    
    Used NodeCanvas so MoneyBug runs away

---
+++ noMad_POC/Assets/Scripts/Tasks/Actions/MoveToWithTimer.cs
@@ -0,0 +1,56 @@
+using NodeCanvas.Framework;
+using ParadoxNotion.Design;
+using Unity.VisualScripting;
+using UnityEngine;
+
+
+namespace NodeCanvas.Tasks.Actions
+{
+
+    public class MoveToWithTimer : ActionTask
+    {
+
+        public float moveSpeed;
+        public BBParameter<float> defaultMoveSpeed;
+        public BBParameter<float> turnSpeed;
+        public BBParameter<Transform> location;
+        public BBParameter<Transform> theHomie;
+        public BBParameter<float> speedBoostDistance;
+        public BBParameter<float> boostedMoveSpeed;
+        public BBParameter<float> stopDistance;
+
+        protected override void OnExecute()
+        {
+            //used to speed up player and ally NPC
+            boostedMoveSpeed.value = moveSpeed * 2;
+        }
+
+        protected override void OnUpdate()
+        {
+            Vector3 direction = SteeringUtility.Flee(agent.transform.position, location.value.position);
+            agent.transform.Translate(moveSpeed * Time.deltaTime * direction, Space.World);
+
+            Quaternion desiredRotation = Quaternion.LookRotation(direction);
+            agent.transform.rotation = Quaternion.Lerp(agent.transform.rotation, desiredRotation, turnSpeed.value * Time.deltaTime);
+
+
+
+            if (Vector3.Distance(agent.transform.position, theHomie.value.position) > stopDistance.value)
+            {
+                //When close enough both the player and NPC gain a movement boost
+                moveSpeed = boostedMoveSpeed.value;
+                moveSpeed = Mathf.Clamp(moveSpeed, defaultMoveSpeed.value, 10f);
+
+            }
+            else
+            {
+                moveSpeed = defaultMoveSpeed.value;
+            }
+
+            if (Vector3.Distance(agent.transform.position, location.value.position) > stopDistance.value)
+            {
+                EndAction(true);
+            }
+        }
+    }
+}
