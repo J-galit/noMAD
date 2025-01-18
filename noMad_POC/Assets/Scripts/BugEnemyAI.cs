6f44a277a62b76f313d795333fc2221afad0a393
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-01-18 00:06:57 -0500

    Money Bug AI is Functional
    -Added NodeCanvas
    
    Wander State is not an action task but a regular script
    
    Used NodeCanvas so MoneyBug runs away

--- noMad_POC/Assets/Scripts/BugEnemyAI.cs
+++ noMad_POC/Assets/Scripts/BugEnemyAI.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:05a8ea9cdf46c7a823de2940833c12c357697da6cdaa00c86b5b1b4aff7a21d3
-size 2187
+oid sha256:1182e89d61c516e50d6676f32f87f20b8223e2e447914bffac4e1598d949579e
+size 2104
---
+++ noMad_POC/Assets/Scripts/Tasks/Actions/MoveToWithTimer.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:1aa8dbd59cb281103ed03e07e7024d34fba983f6fffb6eee723264664ea9913f
+size 1898
