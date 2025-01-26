490b98a9fc3d073ec4ff52714e143c0b36bd06b3
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-01-26 00:28:18 -0500

    Player and Predator Health Added
    Player and Predator Health have been added
    
    Predator:
    -has health displayed above
    -is now a prefab
    -slight invincibility after attack to avoid entering trigger twice
    
    Player:
    -Has health and display on screen
    -Heals, not exactly the way we said it would be implemented but i think its okay for now

--- noMad_POC/Assets/Scripts/BugEnemyAI.cs
+++
@@ -1,3 +0,0 @@
-version https://git-lfs.github.com/spec/v1
-oid sha256:c77a328b50183b8f6e0e981fcd0f7e8c0474a5cc059ca38a7f80fb196530d1c8
-size 1944
---
+++ noMad_POC/Assets/Scripts/PredatorEnemyHealth.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:597929d5fa9c859506e480028cd6eb7477b9b1019f8a7861e8b3a83daa6c2e96
+size 990
