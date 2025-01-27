e7305388eb766ab6e9448492b85f6b3c741306c3
J-galit <jaygalita@gmail.com>
2025-01-27 16:43:59 -0500

    Might have fixed the player input bug.
    Made the program assign the handler in the start function instead of awake.

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d55e37472413b1ab74be26330db845da71f6935ef806c844304edb921473d56d
-size 11020
+oid sha256:75ad2e71554a5cb755c4e8a9f6ba8740315475dbe2611fea9edc23589a19c901
+size 11072
