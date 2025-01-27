9171a0564704c2aabc5df0d3a231640d0dc3d2fa
J-galit <jaygalita@gmail.com>
2025-01-27 16:43:59 -0500

    Might have fixed the player input bug.
    Made the program assign the handler in the start function instead of awake.

--- noMad_POC/Assets/Scripts/PlayerInputHandler.cs
+++ noMad_POC/Assets/Scripts/PlayerInputHandler.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:570f191c65d961cee4b0c2ae732c2358920b8fb163c0bd34afaed6a831fb81ac
-size 3129
+oid sha256:0ad0160fe98b699034bcb816d9d48062c0344959360b8691641a085c684345b7
+size 3169
