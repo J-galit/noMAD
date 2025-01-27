30a0eb7e615d4cb503939b95eed420967366ae92
J-galit <jaygalita@gmail.com>
2025-01-27 17:53:22 -0500

    Revert "Merge branch 'main' of https://github.com/J-galit/noMAD"
    This reverts commit 456b029a3593524a8d395396405efea1da9c7b1a, reversing
    changes made to a736db7022b37db62c64a05b7f9d733f8575026a.

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:13160a8ab660d82e24a88d94e8cd0cde081ca1dce972728fc636e2a3921ed80a
-size 12455
+oid sha256:75ad2e71554a5cb755c4e8a9f6ba8740315475dbe2611fea9edc23589a19c901
+size 11072
