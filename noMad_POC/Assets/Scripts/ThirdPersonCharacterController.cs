8a3f238244b18b9a3561b2f1ff5144b026801738
J-galit <jaygalita@gmail.com>
2025-01-27 17:39:49 -0500

    Revert "Merge branch 'main' of https://github.com/J-galit/noMAD"
    This reverts commit 456b029a3593524a8d395396405efea1da9c7b1a, reversing
    changes made to a736db7022b37db62c64a05b7f9d733f8575026a.

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -13,7 +13,6 @@ public class ThirdPersonCharacterController : MonoBehaviour
     [SerializeField] private GameObject jumpBoostButton;
     [SerializeField] private GameObject speedBoostButton;
     [SerializeField] private GameObject smallSizeButton;
-    [SerializeField] private GameObject largeSizeButton;
 
     [SerializeField] private GameObject maxAdaptationErrorUI;
 
@@ -29,8 +28,6 @@ public class ThirdPersonCharacterController : MonoBehaviour
     private bool isSpeedBoostOwned;
     private int smallerSizeCost = 100;
     private bool isSmallerSizeOwned;
-    private int largerSizeCost = 100;
-    private bool isLargerSizeOwned;
 
 
     [Header("Movement Speeds")]
@@ -58,7 +55,6 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
     [Header("Misc. Adaptations")]
     [SerializeField] private bool isSmallerSizeActive;
-    [SerializeField] private bool isLargerSizeActive;
     [SerializeField] private int totalCurrency;
     [SerializeField] private bool isInDen; //only serialized for debugging
     private bool isAbleToShop = true;
@@ -359,44 +355,6 @@ public class ThirdPersonCharacterController : MonoBehaviour
 
         }
         else if(currentAdaptations >= maxAdaptations)
-        {
-            StartCoroutine(MaxAdaptationCoroutine());
-        }
-        _UICurrency.UpdateCurrency(totalCurrency);
-
-
-    }
-
-    public void LargerSizeButtonHandler()
-    {
-        if (largerSizeCost <= totalCurrency && isLargerSizeOwned == false && currentAdaptations < maxAdaptations)
-        {
-            totalCurrency -= largerSizeCost;
-            isLargerSizeOwned = true;
-        }
-
-        if (isLargerSizeOwned == true)
-        {
-
-            if (isLargerSizeActive == false)
-            {
-                isLargerSizeActive = true;
-                largeSizeButton.SetActive(true);
-                this.transform.localScale = new Vector3(1.5f, 1.5f, 1.5f);
-                currentAdaptations++;
-            }
-            else if (isLargerSizeOwned == true)
-            {
-                isLargerSizeActive = false;
-                largeSizeButton.SetActive(false);
-                this.transform.localScale = Vector3.one;
-                totalCurrency += smallerSizeCost / 2;
-                isLargerSizeOwned = false;
-                currentAdaptations--;
-            }
-
-        }
-        else if (currentAdaptations >= maxAdaptations)
         {
             StartCoroutine(MaxAdaptationCoroutine());
         }
