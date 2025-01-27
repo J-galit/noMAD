948f3662ee7f59f67044e20aa0bd5598357aa966
J-galit <jaygalita@gmail.com>
2025-01-27 17:20:50 -0500

    Changed the UI input device

--- noMad_POC/Assets/Input Actions/ThirdPersonActionsAsset.cs
+++ noMad_POC/Assets/Input Actions/ThirdPersonActionsAsset.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8322a238df327d8e9748a26244e22419f21ad1628392b3fe42d92cd576a0be65
-size 15475
+oid sha256:19c9d96ae7b4cb3554762d8911ab68162471ab7acc1ddd89765cdc323eabcd4d
+size 45497
