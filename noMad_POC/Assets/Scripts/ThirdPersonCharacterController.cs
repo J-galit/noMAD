00ecf5a30003258e90066211c8c8dffbbf121f74
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-01-30 19:35:33 -0500

    Larger Attack Adaptation Implemented
    Player can now buy the adaptation that increases their attack size

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:8f12f0f2f8c14f9e5522cef04551375364d05e92eb72dac709c3eb9b85f877a6
-size 13086
+oid sha256:3ab6ed3229c5cea341d1a80ec75cc5a933a6b21c0948edccbbd1ef511983d2c3
+size 14504
