8f412ab159112f09c5d7ed4d24f4bf27024c552d
pixl3 <skcstudent2018@gmail.com>
2025-02-01 14:17:47 -0500

    non-working jump
    jump sound effect lines are in, but jump button doesn't work nor do the effects. (removing the two lines causes jumping to be fully functional again)

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:d182b2b27f96b98a6771c1042371da0aa9129632006e435d0c50cf554eb07bb9
-size 17806
+oid sha256:f20005ef34a32af9d14c236003bf3887db6f1fa172eb229f733f8332d62cd812
+size 18054
