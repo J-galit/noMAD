231a631bf7ff76dfb7662348158af64471dca77f
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-02-01 16:39:13 -0500

    Faster Healing Adaptation Implemented + BUG FOUND
    Faster healing works as intended
    
    Bug: commented out the code that causes this, if player opens shop, they become immune to damage. Has something to do with turning the character controller off and on again, I am unsure why it causes this bug, I just know that's what is causing it

--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:f20005ef34a32af9d14c236003bf3887db6f1fa172eb229f733f8332d62cd812
-size 18054
+oid sha256:832aef63e4fed5a6b31f7e62dfaaf19b366231d40dd901fd56d5bf3d17b45403
+size 17885
