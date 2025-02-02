4fea6f1219ee76612c78f78a413df57245a5e327
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-02-01 23:58:19 -0500

    commented code

--- noMad_POC/Assets/Scripts/DestroyPlayerAttack.cs
+++ noMad_POC/Assets/Scripts/DestroyPlayerAttack.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e471b684cdd5103c09dc5406c9bf79390771cd619114e601fe33f02f1a9f9f5a
-size 353
+oid sha256:fb009fb763e052a40623af4479d00b147c3c7dbf8408982abaa0395234a16465
+size 403
--- noMad_POC/Assets/Scripts/PlayerHealth.cs
+++ noMad_POC/Assets/Scripts/PlayerHealth.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c08281f939026abee8bf4539e8ffd40b329758ed8b6e7a404274f0ba1f2ef7d1
-size 2604
+oid sha256:13ea6d8d4f15db6480362d633dbf41923703af7c159d978ffb44f151ab20e770
+size 3358
