10e476f18d03c97a311c533a1577c8c383e2f85d
MaacReat <127222403+MaacReat@users.noreply.github.com>
2025-02-01 19:15:51 -0500

    Added Audio Juice To Enemies
    Thought this was the easiest way to add juice

---
+++ noMad_POC/Assets/Scripts/DestroyPlayerAttack.cs
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:e471b684cdd5103c09dc5406c9bf79390771cd619114e601fe33f02f1a9f9f5a
+size 353
--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:bbcba6c6765f9c77ab92e777d8ef931d6144ac3e65ea37277feb87f57d5b78b3
-size 19420
+oid sha256:1d613dca68c0080afe5a0710a09516b830b94a017a855744cb701ec446c84ce0
+size 19440
