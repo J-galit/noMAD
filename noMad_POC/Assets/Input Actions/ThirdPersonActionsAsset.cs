3eb66d65ec79aad2cf7c8e79e168cc52282e563b
J-galit <jaygalita@gmail.com>
2025-02-10 18:18:49 -0500

    Slowed down the camera

--- noMad_POC/Assets/Input Actions/ThirdPersonActionsAsset.cs
+++ noMad_POC/Assets/Input Actions/ThirdPersonActionsAsset.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:5581fa9af4963b920561d862a4738f24d97abb13d110e6b28c7d6691cfde0ca9
-size 45496
+oid sha256:66b2b4ca6c6712b4fad1b995ceacf974324de793a58187e8400ded8942bf7bb1
+size 45521
