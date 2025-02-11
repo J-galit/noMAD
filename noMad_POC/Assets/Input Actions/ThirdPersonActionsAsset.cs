7be92840f19b755226275a47c4811d46e91a135c
J-galit <jaygalita@gmail.com>
2025-02-10 19:37:36 -0500

    Adjusted the camera

--- noMad_POC/Assets/Input Actions/ThirdPersonActionsAsset.cs
+++ noMad_POC/Assets/Input Actions/ThirdPersonActionsAsset.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:66b2b4ca6c6712b4fad1b995ceacf974324de793a58187e8400ded8942bf7bb1
-size 45521
+oid sha256:56f26b68c967c73e855fe8e4b40c688f6629e806be2fd0ad3cced8a1d40ac189
+size 45515
--- noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
+++ noMad_POC/Assets/Scripts/ThirdPersonCharacterController.cs
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:33e4658d424a345076cbfe49f79334e6a6f15f0fd6abfd99763886185879b9af
-size 22155
+oid sha256:1f64e5850188aeab358e87aa57f9cf52cf135907c394bd6de15f6cd50ea1c5a3
+size 22220
